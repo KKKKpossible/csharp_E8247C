80a5523389543e9be13437fc679ec93a4d2db8b2
sjkim <ksj10111011@gmail.com>
2021-03-26 15:44:34 +0900

    기반, 파생 클래스 생성

--- E8247C_0/E8247C_0/Form1.Designer.cs
+++ E8247C_0/E8247C_0/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace E8247C_0
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1343, 695);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ E8247C_0/E8247C_0/src/hw/hw_base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E8247C_0.src.hw
+{
+    class hw_base
+    {
+    }
+}
